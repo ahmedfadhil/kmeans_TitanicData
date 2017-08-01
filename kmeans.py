e52f49bfdaf3824ff7e8a46a0b7ec259bb5b0a6f
ahmedfadhil <ahmed.fadhil@alumni.emu.edu.tr>
2017-08-01 22:23:15 +0200

    kmeans trained and accuracy obtained

--- kmeans.py
+++ kmeans.py
@@ -1,10 +1,11 @@
-#https://pythonprogramming.net/static/downloads/machine-learning-data/titanic.xls
+# https://pythonprogramming.net/static/downloads/machine-learning-data/titanic.xls
 import matplotlib.pyplot as plt
 from matplotlib import style
+
 style.use('ggplot')
 import numpy as np
 from sklearn.cluster import KMeans
-from sklearn import preprocessing, cross_validation
+from sklearn import preprocessing
 import pandas as pd
 
 '''
@@ -25,16 +26,19 @@ home.dest Home/Destination
 '''
 
 df = pd.read_excel('titanic.xls')
-#print(df.head())
-df.drop(['body','name'], 1, inplace=True)
+# print(df.head())
+df.drop(['body', 'name'], 1, inplace=True)
 df.convert_objects(convert_numeric=True)
 df.fillna(0, inplace=True)
-#print(df.head())
+
+
+# print(df.head())
 
 def handle_non_numerical_data(df):
     columns = df.columns.values
     for column in columns:
         text_digit_vals = {}
+
         def convert_to_int(val):
             return text_digit_vals[val]
 
@@ -45,11 +49,31 @@ def handle_non_numerical_data(df):
             for unique in unique_elements:
                 if unique not in text_digit_vals:
                     text_digit_vals[unique] = x
-                    x+=1
+                    x += 1
 
             df[column] = list(map(convert_to_int, df[column]))
 
     return df
 
+
 df = handle_non_numerical_data(df)
-print(df.head())
+# print(df.head())
+
+df.drop(['boat'], 1, inplace=True)
+X = np.array(df.drop(['survived'], 1).astype(float))
+
+y = np.array(df['survived'])
+
+clf = KMeans(n_clusters=2)
+
+clf.fit(X)
+correct = 0
+for i in range(len(X)):
+    predict_me = np.array(X[i].astype(float))
+    X = preprocessing.scale(X)
+    predict_me = predict_me.reshape(-1, len(predict_me))
+    predictions = clf.predict(predict_me)
+
+    if predictions[0] == y[i]:
+        correct += 1
+print(correct / len(X))
